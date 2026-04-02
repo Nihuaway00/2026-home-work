0e8ccfb9484bfc12a6ba378bd5f5a925c6c876c8
katty-gvr <katya-3105@yandex.ru>
2026-04-02 09:59:39 +0300

    Add file dao realisation

--- src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KVServiceFactoryArgumentsProvider.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import company.vk.edu.distrib.compute.gavrilova_ekaterina.InMemoryKVServiceFactory;
+import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
 import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,7 +14,7 @@ import org.junit.platform.commons.util.ReflectionUtils;
 
 public class KVServiceFactoryArgumentsProvider implements ArgumentsProvider {
     private final Set<Class<? extends KVServiceFactory>> factories = Set.of(
-            //KVServiceFactoryImpl.class,
+            KVServiceFactoryImpl.class,
             InMemoryKVServiceFactory.class
     );
 
